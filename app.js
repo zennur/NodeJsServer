02bfd7aa100c9efa5daf445fc5a69f6742bb726e
adison777 <adison777@naver.com>
2016-12-07 11:42:00 +0900

    zehra

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var kooru = require('./routes/kooru');
+var zehra = require('./routes/zehra');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/kooru', kooru);
+app.use('/zehra', zehra);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
